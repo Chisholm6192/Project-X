b212d406652544d38ce5225c6d93c6cddfdb3317
Patrick Gatal <62809624+PatrickIV@users.noreply.github.com>
2022-10-01 16:28:30 -0400

    backend stuff

--- project-x.py
+++ project-x.py
@@ -1,7 +1,21 @@
+from flask import Flask
+import flask
+app = Flask(__name__)
+import json
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
+@app.route('/users', methods=["GET"])
+def users():
+    print("users endpoint reached...")
+    with open("users.json", "r") as f:
+        data = json.load(f)
+        data.append({"username":"user4", "pets": ["hamster"]})
+        return flask.jsonify(data)
+if __name__ == '__main__':
+    app.run('localhost', 6969)
 
-
-dgjkvsdk
 test_inputID = '1241 4324 3453 5325'
 test_inputExpiryFrom = '05/21'
 test_inputExpiryThru = '09/24'
